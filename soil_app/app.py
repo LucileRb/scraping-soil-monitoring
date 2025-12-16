76f64bed397cb5a18003c424adac47e6cedc8c68
LucileRb <lucile.rabeau@gmail.com>
2025-12-16 10:54:40 +0100

    aiofha

--- soil_app/app.py
+++ soil_app/app.py
@@ -38,14 +38,25 @@ app_mode = st.sidebar.selectbox('Choix de la page', [
 if app_mode == 'Home':
     st.title('BLABLABLA')
     st.divider()
-    phrase = '''
-    Hello,
-    gnagnagnagnagnagnagnagnagnagnagnagna
-
-    mettre abstract de l'article ici ? ou au moins résumé / explication de l'outils
-    '''
-    st.subheader(phrase)
+    st.subheader("Hello, gnagnagnagnagnagnagnagnagnagnagnagna - mettre abstract de l'article ici ? ou au moins résumé / explication de l'outils")
     st.divider()
+    st.image('scoring_app/app_illustrations/paying-off-a-loan-early.jpg')
+
+    # à défaut d'afficher infos sur jeu de données, décrire les features utilisées:
+    st.subheader("Liste des descripteurs utilisés pour prédire gnagna :")
+
+    st.write("- :blue[CNT_CHILDREN]     :     Nombre d'enfants qu'à le/la client(e)")
+    st.write("- :blue[CNT_FAM_MEMBERS]     :     Nombre de membres dans la famille")
+    st.write("- :blue[PREVIOUS_LOANS_COUNT]     :     Nombre total des précédents crédits pris par chaque client")
+    st.write("- :blue[NONLIVINGAREA_MODE]     :     Informations normalisées sur l'immeuble où vit le client (taille, étages, etc...)")
+    st.write("- :blue[AMT_REQ_CREDIT_BUREAU_QRT]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client 3 mois avant la demande (à l'exclusion du mois précédant la demande)")
+    st.write("- :blue[AMT_REQ_CREDIT_BUREAU_YEAR]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client sur un an (à l'exclusion des 3 derniers mois avant la demande)")
+    st.write("- :blue[EXT_SOURCE_3]     :     Score normalisé provenant d'une source de données externe.")
+    st.write("- :blue[OBS_30_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client avec un défaut observable de 30 jours de retard (30 DPD).")
+    st.write("- :blue[OBS_60_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client avec un défaut observable de 60 jours de retard (30 DPD).")
+    st.write("- :blue[DEF_30_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client ont fait défaut avec un retard de paiement de 30 jours (30 DPD)")
+
+
 
 elif app_mode == 'Pokedex':
     st.title('POKEDEX')
