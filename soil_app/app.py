dc8c2c4e4f7fdadd54669f6f758cdeb0aa2af020
LucileRb <lucile.rabeau@gmail.com>
2025-12-16 10:44:44 +0100

    hafhkhgef

--- soil_app/app.py
+++ soil_app/app.py
@@ -26,5 +26,30 @@ st.set_page_config(
     layout = 'wide'
     )
 
+# Définition des pages de l'application
+st.sidebar.image('soil_app/app_illustrations/image_soil.png')
+app_mode = st.sidebar.selectbox('Choix de la page', [
+    'Home', # page d'accueil et description des variables
+    'Pokedex', # page pour visualiser les groupes de MRVs
+    'Decision tool' # page de l'outils d'aide à la décision
+    ])
+
+if app_mode == 'Home':
+    st.title('BLABLABLA')
+
+elif app_mode == 'Pokedex':
+    st.title('POKEDEX')
+
+elif app_mode == 'Decision tool':
+    st.title('OUTILS DE PREDICTION')
+    st.divider()
+    phrase = '''
+    Bonjour,
+    merci de remplir les informations suivantes à propos du client afin de déterminer si nous devons acceder à sa demande de prêt
+    '''
+    st.subheader(phrase)
+    st.divider()
+
+    st.sidebar.header('Informations à propos du nouveau client:')
 
 ################################################################################################ END ################################################################################################
